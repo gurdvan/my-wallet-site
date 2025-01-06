78e64137bb5b80e357916e1f32fa70f0286bdc60
gurdvan <gurdvan@gmail.com>
2025-01-06 20:33:16 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+app.use(cors());
 const app = express();
 
 // فعال‌سازی CORS برای درخواست‌های از دامنه‌های مختلف
